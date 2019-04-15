d5f203de78747e29d7a62a70e905230f3b6f067c
ondrejdubaj <ondrej.dubaj@gmail.com>
2019-04-15 23:31:19 +0200

    added new route tests, created other uni test categories

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -11,12 +11,6 @@ class RouteTest extends TestCase
 {
     public function testRoutes()
     {
-        $response = $this->get('/projekty');
-        if(\Auth::check())
-            $response->assertStatus(200);
-        else
-            $response->assertStatus(302);
-
         $response = $this->get('/login');
         $response->assertStatus(200);
 
@@ -24,6 +18,78 @@ class RouteTest extends TestCase
         $response->assertStatus(200);
 
         $response = $this->get('/home');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/uzivatele');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/uzivatel/1');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/uzivatele/upravit/1');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/uzivatele/vytvorit');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/projekty');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/projekty/vytvorit');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/projekt/1');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('projekty/upravit/1');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('projekty/status/upravit/1');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/projekty/faze/vsechny/1');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('/projekty/faze/upravit/1');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+
+        $response = $this->get('projekty/faze/zmenit/1');
         if(\Auth::check())
             $response->assertStatus(200);
         else
