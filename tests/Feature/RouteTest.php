c9e9abf72a9fc73e61d68509b426a06fbb8d6b9d
ondrejdubaj <ondrej.dubaj@gmail.com>
2019-04-15 17:28:50 +0200

    initialation of tests, first tests on routes

---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Auth;
+
+class RouteTest extends TestCase
+{
+    public function testRoutes()
+    {
+        $response = $this->get('/projekty');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(200);
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+
+        $response = $this->get('/home');
+        if(\Auth::check())
+            $response->assertStatus(200);
+        else
+            $response->assertStatus(302);
+    }
+}
