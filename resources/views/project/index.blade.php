3164856151906d770b86ce511deac2f4d4a675c7
Marek Schauer <marek.schauer@psoit.sk>
2019-04-21 18:22:55 +0000

    Project filtering

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -4,6 +4,50 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <form {{-- class="form-inline" --}}>
+                    
+                    <div class="form-row">
+                        <div class="col">
+                            <label class="" for="project-manager">Projektový manažer</label>
+                            <select class="custom-select" name="project_manager" id="project-manager">
+                                <option value="-1">Nezvoleno</option>
+                                @foreach($managers as $manager)
+                                <option value="{{$manager->id}}" @if(array_key_exists('project_manager', $filters) && $filters['project_manager'] == $manager->id) selected="selected=" @endif >{{$manager->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="col">
+                            <label class="" for="actual-phase">Aktuální fáze</label>
+                            <select class="custom-select" name="actual_phase" id="actual-phase">
+                                <option value="-1">Nezvoleno</option>
+                                @foreach($phases as $phase)
+                                <option value="{{$phase->id}}" @if(array_key_exists('actual_phase', $filters) && $filters['actual_phase'] == $phase->id) selected="selected=" @endif >{{$phase->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="col">
+                            <label class="" for="project-name">Název projektu</label>
+                            <div class="input-group mb-2 mr-sm-2">
+                                <input type="text" class="form-control" id="project-name" name="project_name" placeholder="Název" @if(array_key_exists('project_name', $filters)) value="{{$filters['project_name']}}" @endif>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <label class="" for="project-status">Status projektu</label>
+                            <div class="input-group mb-2 mr-sm-2">
+                                <input type="text" class="form-control" id="project-status" name="project_status" placeholder="Status" @if(array_key_exists('project_status', $filters)) value="{{$filters['project_status']}}" @endif>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mb-2 float-right">Submit</button>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <a class="btn btn-primary" href="{{ route('project.create') }}">
@@ -61,7 +105,7 @@
                             </tbody>
                         </table>
                     </div>
-                    {{ $projects->links() }}
+                    {{ $projects->appends($filters)->links() }}
                 </div>
             </div>
         </div>
